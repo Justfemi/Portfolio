dc197235ae8f4c6aea77b6b078b30afc57406778
Justfemi <ajibadeolufemi94@gmail.com>
2022-02-27 07:00:24 +0100

    bugs:fixed error and added images:

--- script.js
+++ script.js
@@ -26,30 +26,30 @@
 
 // REAVEALING FUNCTIONALITY
 
-// const sr = ScrollReveal({
-//     origin: 'top',
-//     distance: '80px',
-//     duration: 2000,
-//     reset: true
-// })
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
 
-// sr.reveal('.home__title', {})
-// sr.reveal('.button', {delay: 200})
-// sr.reveal('.home__img', {delay: 400})
-// sr.reveal('.home__social-icon', {interval: 200})
+sr.reveal('.home__title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home__img', {delay: 400})
+sr.reveal('.home__social-icon', {interval: 200})
 
-// sr.reveal('.about__img', {})
-// sr.reveal('.about__subtitle', {delay: 200})
-// sr.reveal('.about__text', {delay: 400})
+sr.reveal('.about__img', {})
+sr.reveal('.about__subtitle', {delay: 200})
+sr.reveal('.about__text', {delay: 400})
 
-// sr.reveal('.skills__subtitle', {})
-// sr.reveal('.skills__text', {delay: 200})
-// sr.reveal('.skills__data', {interval: 200})
-// sr.reveal('.skills__img', {delay: 400})
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__text', {delay: 200})
+sr.reveal('.skills__data', {interval: 200})
+sr.reveal('.skills__img', {delay: 400})
 
-// sr.reveal('.work__img', {interval: 200})
+sr.reveal('.work__img', {interval: 200})
 
-// sr.reveal('.contact__input', {interval: 200})
+sr.reveal('.contact__input', {interval: 200})
 
 
 // WORK SECTION CARDS
