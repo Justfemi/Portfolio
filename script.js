77f9e6639cd66951803343579a88bfdc0dc95e5a
Justfemi <ajibadeolufemi94@gmail.com>
2022-03-14 15:22:01 +0100

    chnaged cards to carousel

--- script.js
+++ script.js
@@ -3,47 +3,47 @@
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 2000,
+    duration: 1000,
     reset: true
 })
 
 sr.reveal('.home__title', {})
-sr.reveal('.button', {delay: 200})
-sr.reveal('.home__img', {delay: 400})
-sr.reveal('.home__social-icon', {interval: 200})
+sr.reveal('.button', {delay: 100})
+sr.reveal('.home__img', {delay: 200})
+sr.reveal('.home__social-icon', {interval: 100})
 
 sr.reveal('.about__img', {})
-sr.reveal('.about__subtitle', {delay: 200})
-sr.reveal('.about__text', {delay: 400})
+sr.reveal('.about__subtitle', {delay: 100})
+sr.reveal('.about__text', {delay: 200})
 
 sr.reveal('.skills__box', {})
-sr.reveal('.skills__box .title', {delay: 200})
-sr.reveal('.skills__items', {interval: 200})
+sr.reveal('.skills__box .title', {delay: 100})
+sr.reveal('.skills__items', {interval: 100})
 
 sr.reveal('.work__body', {})
-sr.reveal('.work__container', {delay: 200})
-sr.reveal('.work__panel', {interval: 200})
+sr.reveal('.work__container', {delay: 100})
+sr.reveal('.work__panel', {interval: 100})
 
-sr.reveal('.contact__input', {interval: 200})
+sr.reveal('.contact__input', {interval: 100})
 
 
 // WORK SECTION CARDS
 
-const panels = document.querySelectorAll('.work__panel');
+// const panels = document.querySelectorAll('.work__panel');
 
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        console.log('panel clicked');
-        panel.classList.add('active');
-    });
-});
+// panels.forEach((panel) => {
+//     panel.addEventListener('click', () => {
+//         removeActiveClasses();
+//         console.log('panel clicked');
+//         panel.classList.add('active');
+//     });
+// });
 
-function removeActiveClasses() {
-    panels.forEach((panel) => {
-        panel.classList.remove('active');
-    });
-}
+// function removeActiveClasses() {
+//     panels.forEach((panel) => {
+//         panel.classList.remove('active');
+//     });
+// }
 
 // TOGGLE NAV ITEM ACTIVE
 // let menu = document.querySelector('.nav__menu');
@@ -72,5 +72,34 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('show');
     });
+});
+
+// SWIPER FOR PROJECTS SECTION 
+let swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
 });
  
